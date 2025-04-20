41d1bee2fdeda60aaf4215ccb5199fdeb05e6ccf
Alisuuu <132578134+Alisuuu@users.noreply.github.com>
2025-04-20 12:30:22 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ export default function Home() {
   const [news, setNews] = useState([]);
   const [selected, setSelected] = useState(null);
 
-  // Carrega as notícias assim que o componente for montado
   useEffect(() => {
     axios.get('/api/news')
       .then(res => {
@@ -19,12 +18,11 @@ export default function Home() {
     <div style={{ padding: '2rem', backgroundColor: '#000', height: '100vh', overflow: 'hidden' }}>
       <h1 style={{ color: '#593BA2', textAlign: 'center' }}>Últimas Notícias</h1>
 
-      {/* Lista de notícias */}
       <div style={{ display: 'grid', gap: '1rem', marginTop: '2rem', gridTemplateColumns: 'repeat(auto-fit, minmax(280px, 1fr))' }}>
         {news.map((n, i) => (
           <div
             key={i}
-            onClick={() => setSelected(n)} // Abre o modal com a notícia selecionada
+            onClick={() => setSelected(n)}
             style={{
               backgroundColor: 'rgb(12, 12, 12, 0.2)',
               padding: '1rem',
@@ -40,11 +38,10 @@ export default function Home() {
         ))}
       </div>
 
-      {/* Modal com detalhes da notícia */}
       <Modal
         isOpen={!!selected}
         article={selected}
-        onClose={() => setSelected(null)} // Fecha o modal
+        onClose={() => setSelected(null)}
       />
     </div>
   );
